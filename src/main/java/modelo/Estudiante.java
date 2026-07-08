f3aafd9466fba17027ef9410b3b13cb76417500d
jvillafuertenajera-ops <jvillafuertenajera@gmail.com>
2026-07-08 14:48:35 -0600

    Se crea la clase Estudiante con herencia de Usuario
    Se agrega clase estudiante

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -6,8 +6,42 @@ package modelo;
 
 /**
  *
- * @author David Cruz
+ * @author jorge.villafuerte
  */
 public class Estudiante extends Usuario {
-    
+
+    private String carrera;
+    private String carnet;
+
+    // Constructor vacío
+    public Estudiante() {
+    }
+
+    // Constructor con parámetros
+    public Estudiante(String carrera, String carnet) {
+        this.carrera = carrera;
+        this.carnet = carnet;
+    }
+
+    // Getter de carrera
+    public String getCarrera() {
+        return carrera;
+    }
+
+    // Setter de carrera
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    // Getter del carnet
+    public String getCarnet() {
+        return carnet;
+    }
+
+    // Setter del carnet
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
 }
+
