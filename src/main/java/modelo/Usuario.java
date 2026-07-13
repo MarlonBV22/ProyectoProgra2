ce3db6636f7e3decc53a344c6634125f82ec5da1
MarlonBV22 <marlonbolanosvillalobos@gmail.com>
2026-07-13 14:04:02 -0600

    ProyectoProgra2

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -9,13 +9,13 @@ public class Usuario {
  private String password;
  private int fechaRegistro;
  
- //constructor con parametros
-    public Usuario(int idUsuarioP, String nombreP, String emailP, String passwordP, int fechaRegistroP) {
-        this.idUsuario = idUsuarioP;
-        this.nombre = nombreP;
-        this.email = emailP;
-        this.password = passwordP;
-        this.fechaRegistro = fechaRegistroP;
+//constructor con parametros
+    public Usuario(int idUsuario, String nombre, String email, String password, int fechaRegistro) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.fechaRegistro = fechaRegistro;
     }
 // contructor sin paramatros 
     public Usuario() {
@@ -26,40 +26,40 @@ public class Usuario {
         return idUsuario;
     }
 
-    public void setIDusuario(int idUsuarioP) {
-        this.idUsuario = idUsuarioP;
+    public void setIDusuario(int idUsuario) {
+        this.idUsuario = idUsuario;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombreP) {
-        this.nombre = nombreP;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String emailP) {
-        this.email = emailP;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String passwordP) {
-        this.password = passwordP;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public int getFechaRegistro() {
         return fechaRegistro;
     }
 
-    public void setFechaRegistro(int fechaRegistroP) {
-        this.fechaRegistro = fechaRegistroP;
+    public void setFechaRegistro(int fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
     }
    
 }
