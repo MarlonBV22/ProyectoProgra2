872aafc50be0aa6a35a26a4f10a08c20571fa57f
Davidcruzalf0510 <davidcruzalf05@gmail.com>
2026-07-26 09:15:02 -0600

    ProyectoProgramacion2
    queda listo la clase estudiante con todo set get y variables

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -3,6 +3,9 @@ package modelo;
 import java.time.LocalDateTime; // Importación necesaria para el TIMESTAMP
 
 public class Estudiante extends Usuario {
+    private String nombreString;
+    private String email;
+    private String password;
 
     // Constructor con parametros
     public Estudiante(int idUsuario,
@@ -16,4 +19,31 @@ public class Estudiante extends Usuario {
     // Constructor sin parametros
     public Estudiante(){
     }
+    
+    //creacion de gets y sets
+
+    public String getNombreString() {
+        return nombreString;
+    }
+
+    public void setNombreString(String nombreString) {
+        this.nombreString = nombreString;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
